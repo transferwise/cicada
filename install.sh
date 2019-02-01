8f1c621f30a1491683a3eded25157a6175ca1948
Louis Pieterse <louis.pieterse@transferwise.com>
2019-02-01 10:45:13 +0000

    Improve Install script

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 DIR=$(pwd)
+VENV_DIR=$DIR/.virtualenvs
 
 # Deploy cicada-scheduler in Python3 virtual environment
-python3 -m venv .virtualenvs
-source $DIR/.virtualenvs/bin/activate
+python3 -m venv $VENV_DIR
+source $DIR/$VENV_DIR/bin/activate
 python3 -m pip install --upgrade pip
 python3 -m pip install croniter psycopg2-binary pyyaml
 deactivate
@@ -14,6 +15,6 @@ echo "--------------------------------------------------------------------------
 echo
 echo "1. cp $DIR/config/example.yml $DIR/config/definitions.yml"
 echo "2. vim $DIR/config/definitions.yml"
-echo "3. echo \"* * * * * $DIR/.virtualenvs/bin/python3 $DIR/bin/findSchedules.py\" | crontab "
-echo "4. $DIR/.virtualenvs/bin/python3 $DIR/bin/registerServer.py"
+echo "3. echo \"* * * * * $DIR/$VENV_DIR/bin/python3 $DIR/bin/findSchedules.py\" | crontab "
+echo "4. $DIR/$VENV_DIR/bin/python3 $DIR/bin/registerServer.py"
 echo
