4165988f1050237377772ec2dc168073e40b9535
Louis Pieterse <louis.pieterse@transferwise.com>
2019-02-06 14:54:41 +0000

    add last run_duration

--- reports/schedules_latest_status.sql
+++ reports/schedules_latest_status.sql
@@ -5,29 +5,12 @@ SELECT
   server_name,
   schedule_id,
   schedule_description,
+  is_enabled,
   start_time,
   end_time,
+  run_duration,
   schedule_status
 FROM
-
-( /* foo */
-SELECT
-  servers.server_id AS server_id,
-  servers.hostname AS server_name,
-  schedule_id,
-  description AS schedule_description,
-  start_time,
-  end_time,
-  schedule_log_status.name AS schedule_status
-FROM servers
-  INNER JOIN schedules USING (server_id)
-  LEFT JOIN schedule_log USING (schedule_id)
-  LEFT JOIN schedule_log_status USING (schedule_log_status_id)
-WHERE (schedule_group_id IS NULL OR schedule_group_id NOT IN (2))
-ORDER BY schedule_id, start_time
-) AS foo
-
-INNER JOIN
 
 ( /* bar */
 SELECT
@@ -36,6 +19,27 @@ SELECT
 FROM schedule_log
 GROUP BY schedule_id
 ) AS bar
+
+INNER JOIN
+
+( /* foo */
+SELECT
+  servers.server_id AS server_id,
+  servers.hostname AS server_name,
+  schedule_id,
+  description AS schedule_description,
+  is_enabled,
+  start_time,
+  end_time,
+  (end_time - start_time) AS run_duration,
+  schedule_log_status.name AS schedule_status
+FROM schedules
+  INNER JOIN servers USING (server_id)
+  INNER JOIN schedule_log USING (schedule_id)
+  INNER JOIN schedule_log_status USING (schedule_log_status_id)
+WHERE (schedule_group_id IS NULL OR schedule_group_id NOT IN (2))
+ORDER BY schedule_id, start_time
+) AS foo
 
 USING (schedule_id, start_time)
 
