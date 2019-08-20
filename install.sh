808f24cc36e82c63a636c7d6cce84b6bd4ec7026
Louis Pieterse <louis.pieterse@transferwise.com>
2019-08-20 18:32:02 +0100

    local install

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-HOME=/opt/app
-VENV_DIR=$HOME/cicada-scheduler/.virtualenvs
+HOME=.
+VENV_DIR=.virtualenvs
 
 # Deploy cicada-scheduler in Python3 virtual environment
 python3 -m venv $VENV_DIR
@@ -8,14 +8,3 @@ source $VENV_DIR/bin/activate
 python3 -m pip install --upgrade pip
 python3 -m pip install croniter psycopg2-binary pyyaml tabulate
 deactivate
-
-echo
-echo "--------------------------------------------------------------------------"
-echo "Cicada Scheduler Intiated - Next Steps"
-echo "--------------------------------------------------------------------------"
-echo
-echo "cp $HOME/config/example.yml $HOME/config/definitions.yml"
-echo "vim $HOME/config/definitions.yml"
-echo "$VENV_DIR/bin/python3 $HOME/bin/registerServer.py"
-echo "echo \"* * * * * $VENV_DIR/bin/python3 $HOME/bin/findSchedules.py\" | crontab "
-echo
