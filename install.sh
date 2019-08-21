f2e6a96961b16d09a959c50669f29fc149991dc4
Louis Pieterse <louis.pieterse@transferwise.com>
2019-08-21 11:06:15 +0100

    restrict install script

--- install.sh
+++ install.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-HOME=.
-VENV_DIR=.virtualenvs
 
 # Deploy cicada-scheduler in Python3 virtual environment
-python3 -m venv $VENV_DIR
-source $VENV_DIR/bin/activate
+python3 -m venv .virtualenvs
+source .virtualenvs/bin/activate
 python3 -m pip install --upgrade pip
 python3 -m pip install croniter psycopg2-binary pyyaml tabulate
 deactivate
