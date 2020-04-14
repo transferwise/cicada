26fa9da6d5d6928c35777309b464657ec4f0fec4
Louis Pieterse <louis.pieterse@transferwise.com>
2020-04-14 18:09:08 +0200

    cleaner database definition

--- setup/db_and_user.sql
+++ setup/db_and_user.sql
@@ -11,6 +11,4 @@ CREATE DATABASE db_cicada
   WITH OWNER = cicada
        ENCODING = 'UTF8'
        TABLESPACE = pg_default
-       LC_COLLATE = 'en_US.UTF-8'
-       LC_CTYPE = 'en_US.UTF-8'
        CONNECTION LIMIT = -1;
