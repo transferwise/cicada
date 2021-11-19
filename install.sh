0d6c672f48b1310c0abfb3396cd6deca950c9653
Louis Pieterse <louis.pieterse@transferwise.com>
2021-11-19 12:04:17 +0000

    virtualenv -> virtualenvs

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 # Use virtual environment for Python3
 python3.6 -m pip install --upgrade pip
-python3.6 -m venv .virtualenv
-source .virtualenv/bin/activate
+python3.6 -m venv .virtualenvs
+source .virtualenvs/bin/activate
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
 deactivate
