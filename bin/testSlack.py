0215fe4a7497db4196f36eea6adeb3b7ad84e6d2
Louis Pieterse <louis.pieterse@transferwise.com>
2021-11-22 10:51:43 +0000

    Script to test Slack settings and availability

---
+++ bin/testSlack.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+
+import os
+import sys
+import argparse
+
+sys.path.append(os.path.abspath(os.path.dirname(sys.argv[0]) + "/../lib"))
+import libPgSQL
+import libScheduler
+import utils
+
+def main():
+    parser = argparse.ArgumentParser(description='Send a test message to configured Slack channel', add_help=True)
+    parser.add_argument('--text', type=str, nargs='?', const='Some more text', help='Expanded text to send to Slack channel')
+    args = parser.parse_args()
+
+    if args.text is None:
+        text = 'Default text'
+    else:
+        text = args.text
+
+    utils.send_slack_message('Test using Cicada testSlack.py', text, 'good')
+
+if __name__ == '__main__':
+    main()
