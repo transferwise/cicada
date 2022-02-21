6838c5d95db3fc0f4d57e69677810541b420b486
Louis Pieterse <louis.pieterse@transferwise.com>
2022-02-21 09:28:03 +0000

    Updated dependancies

--- setup.py
+++ setup.py
@@ -20,9 +20,9 @@ setup(
     install_requires=[
         'psycopg2-binary==2.9.3',
         'pyyaml==6.0',
-        'croniter==1.2.0',
+        'croniter==1.3.4',
         'tabulate==0.8.9',
-        'slack-sdk==3.14.1',
+        'slack-sdk==3.15.1',
         'backoff==1.11.1',
         'psutil==5.9.0',
     ],
